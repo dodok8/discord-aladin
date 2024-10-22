8219b605c9961c11ce0d3f63a5017f93e10da887
Kim, Hyeonseo <dodok8@gmail.com>
2024-10-22 10:28:51 +0000

    🔥 Remove SlashCommand type

--- src/@types/discord.d.ts
+++
@@ -1,9 +0,0 @@
-import {
-  CommandInteraction,
-  ChatInputApplicationCommandData,
-  Client,
-} from 'discord.js'
-
-export type SlashCommand = ChatInputApplicationCommandData & {
-  execute: (client: Client, interaction: CommandInteraction) => void
-}
