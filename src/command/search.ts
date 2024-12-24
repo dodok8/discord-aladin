20cf98ddcf837d9cc1c55c7783bc4b03ebacfe52
Kim, Hyeonseo <dodok8@gmail.com>
2024-12-24 05:21:42 +0000

    ✨ 검색 명령어에 페이지네이션 버튼 추가

--- src/command/search.ts
+++ src/command/search.ts
@@ -1,4 +1,11 @@
-import { EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ComponentType,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from 'discord.js'
 import { generateUrlQueryForType, removeExtraSpaces, truncate } from '../utils'
 import ky from 'ky'
 import ApplicationCommand from '../@types/ApplicationCommand'
@@ -55,13 +62,15 @@ export const searchCommand = new ApplicationCommand({
     const searchTarget = (interaction.options.getString('검색-대상') ||
       'All') as SearchTarget['value']
 
+    let start = 1
+
     try {
       const { item, totalResults } = await itemSearch(
         count,
         query,
         queryType,
         searchTarget,
-        1
+        start
       )
 
       const bookInfos = item.map((i: any): [string, string] => [
@@ -71,6 +80,19 @@ export const searchCommand = new ApplicationCommand({
         )}`,
         i.link,
       ])
+
+      const nextButton = new ButtonBuilder()
+        .setCustomId('next_page')
+        .setLabel('다음 페이지')
+        .setStyle(ButtonStyle.Primary)
+      const previousButton = new ButtonBuilder()
+        .setCustomId('previous_page')
+        .setLabel('이전 페이지')
+        .setStyle(ButtonStyle.Secondary)
+      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+        previousButton,
+        nextButton
+      )
 
       const embed = new EmbedBuilder()
         .setAuthor({
@@ -96,7 +118,84 @@ export const searchCommand = new ApplicationCommand({
         .setColor('#eb3b94')
         .setTimestamp()
 
-      await interaction.reply({ embeds: [embed] })
+      const response = await interaction.reply({
+        embeds: [embed],
+        components: [row],
+        fetchReply: true,
+      })
+
+      let continueLoop = true
+      while (continueLoop) {
+        try {
+          const { item, totalResults } = await itemSearch(
+            count,
+            query,
+            queryType,
+            searchTarget,
+            start
+          )
+
+          const bookInfos = item.map((i: any): [string, string] => [
+            `${truncate(removeExtraSpaces(i.title), 200)} | ${truncate(
+              removeExtraSpaces(i.author),
+              40
+            )}`,
+            i.link,
+          ])
+
+          const embed = new EmbedBuilder()
+            .setAuthor({
+              name: '알라딘 도서검색',
+              iconURL:
+                'https://image.aladin.co.kr/img/m/2018/shopping_app1.png',
+            })
+            .setTitle(`검색결과 : ${query}`)
+            .setURL(
+              `http://www.aladin.co.kr/search/wsearchresult.aspx?SearchWord=${encodeURIComponent(
+                query
+              )}&SearchTarget=${searchTarget}&${generateUrlQueryForType(
+                queryType
+              )}`
+            )
+            .setDescription(`총 ${totalResults}건 검색`)
+            .addFields(
+              ...bookInfos.map((bookInfo: [string, string]) => {
+                return {
+                  name: bookInfo[0],
+                  value: `[자세히 보기](${bookInfo[1]})`,
+                  inline: false,
+                }
+              })
+            )
+            .setColor('#eb3b94')
+            .setTimestamp()
+
+          const response = await interaction.editReply({
+            embeds: [embed],
+            components: [row],
+          })
+
+          const confirmation = await response.awaitMessageComponent({
+            filter: (i) => i.user.id === interaction.user.id,
+            componentType: ComponentType.Button,
+          })
+
+          if (confirmation.customId === 'next_page') {
+            start += 1
+          } else if (confirmation.customId === 'previous_page') {
+            start -= start > 0 ? 1 : 0
+          }
+
+          await confirmation.deferUpdate()
+        } catch (err) {
+          console.error(err)
+          continueLoop = false
+          await interaction.followUp({
+            content: ':x: 에러가 발생했습니다.',
+            ephemeral: true,
+          })
+        }
+      }
     } catch (err) {
       console.error(err)
       await interaction.reply({
